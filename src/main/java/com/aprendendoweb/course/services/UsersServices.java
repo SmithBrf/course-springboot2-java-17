2480382e6ac6997df15b39bd5454917fe707d534
SmithBrf <fellipev72@gmail.com>
2022-08-07 03:36:19 -0300

    Exception handling - delete

--- src/main/java/com/aprendendoweb/course/services/UsersServices.java
+++ src/main/java/com/aprendendoweb/course/services/UsersServices.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.aprendendoweb.course.entities.Users;
 import com.aprendendoweb.course.repositories.UsersRepository;
+import com.aprendendoweb.course.services.exceptions.DatabaseException;
 import com.aprendendoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UsersServices {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Users update(Long id, Users obj) {
